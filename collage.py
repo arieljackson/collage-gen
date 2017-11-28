f74666395b6687de9ab9966913f9736c50aa8b42
Ariel <arieljackson@users.noreply.github.com>
2017-11-28 15:13:28 -0600

    Commit final website for demo, need to clean up

--- collage.py
+++ collage.py
@@ -12,8 +12,17 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from PIL import Image
+import numpy as np
+import pickle
+import math
+import glob
+import matplotlib.pyplot as plt
+import random
 
 """ Save a screenshot from spotify.com in current directory"""
+website = 'twitter.com/barackobama'
+url = ('https://www.' + website)
 DRIVER = 'chromedriver'
 username = os.getenv("USERNAME")
 userProfile = "C:\\Users\\", username, "\\AppData\\Local\\Google\\Chrome\\User Data\\Default"
@@ -21,60 +30,56 @@ _chrome_options = Options()
 _chrome_options.add_argument('disable-infobars')
 _chrome_options.add_argument("user-data-dir={}".format(userProfile))
 driver = webdriver.Chrome(DRIVER, chrome_options = _chrome_options)
-driver.get('https://www.facebook.com')
-# driver.set_winçow_size(width, height)
+driver.get(url)
 
-def wait_for(condition_function):
-    start_time = time.time()
-    while time.time() < start_time + 3:
-        if condition_function():
-            return True
-        else:
-            time.sleep(0.1)
-    raise Exception(
-        'Timeout waiting for {}'.format(condition_function.__name__)
-    )
-
-def click_through_to_new_page(link, my_id):
-    link.click()
-
-    def link_has_gone_stale():
-        try:
-            # poll the link with an arbitrary call
-            link.find_elements_by_id(my_id) 
-            return False
-        except StaleElementReferenceException:
-            return True
-
-    wait_for(link_has_gone_stale)
-
-
-for i in range(15):
+now = str(datetime.today().timestamp())
+img_dir = ''.join([now, website])
+img_paths = []
+max_runs = 15
+for i in range(max_runs):
 	print("i is", i)
 	# driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-	now = str(datetime.today().timestamp())
-	img_dir = 'testing'
-	img_name = ''.join([now, '_image.png'])
+	img_name = ''.join([str(i), '_image.png'])
 	full_img_path = os.path.join(img_dir, img_name)
+	img_paths.append(full_img_path)
 	if not os.path.exists(img_dir):
 	    os.makedirs(img_dir)
 	driver.save_screenshot(full_img_path)
 	screenshot = open(full_img_path, 'rb').read()
 	var_dict = {'screenshot': img_name, 'save': True}
 	# links = driver.find_elements_by_tag_name('a')
-	links = driver.find_elements_by_xpath("//a[@href]")
-	num_links = len(links)
-	print(num_links)
-	index = math.floor((i / 15) * num_links)
-	print(index)
-	# attribute = links[3].get_attribute('id'); 
-	attribute = links[index].get_attribute("href")
-	print(attribute) 
-	# driver.maximize_window()
-	driver.get(attribute)
-	# click_through_to_new_page(links[0], attribute)
-	# timeout = 5
-	# element_present = EC.presence_of_element_located((By.TAG, 'a'))
-	# WebDriverWait(driver, timeout).until(element_present)
+	try:
+		links = driver.find_elements_by_xpath("//a[@href]")
+		num_links = len(links)
+		print(num_links)
+		if num_links == 0:
+			break
+		index = math.floor((i / max_runs) * num_links)
+		if (index == num_links):
+			index = index-1
+		print(index)
+		attribute = links[index].get_attribute("href")
+		print(attribute) 
+		driver.get(attribute)
+	except:
+		break
+
+imagelist = [Image.open(path) for path in img_paths]
+width = 1200
+height = 1000
+new_im = Image.new('RGB', (width, height))
+for i, image in enumerate(imagelist):
+	if i == 0:
+		denom = 2
+	else:
+		denom = max_runs + 1 - i
+	resized_width = width // denom
+	resized_height = height // denom
+	x = random.randint(0, width - resized_width)
+	y = random.randint(0, height - resized_height)
+	resized = image.resize([resized_width,resized_height], Image.ANTIALIAS)
+	new_im.paste(resized, (x, y))
+
+new_im.save("Collage.jpg")
 driver.quit()
 
--- collage_generator/app/views.py
+++ collage_generator/app/views.py
@@ -1,25 +1,103 @@
+from selenium import webdriver
+from selenium.common.exceptions import StaleElementReferenceException
+import base64
+import os
+import math
+import urllib.parse as urlparse
+from datetime import datetime
+import time
+from selenium.webdriver.chrome.options import Options
+from PIL import Image
+import math
+import glob
+import random
+
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
+DRIVER = 'chromedriver'
 
 # views.py
 def get_screenshot(request):
-    width = 1024
-    height = 768
+
+
+    username = os.getenv("USERNAME")
+    userProfile = "C:\\Users\\", username, "\\AppData\\Local\\Google\\Chrome\\User Data\\Default"
+    _chrome_options = Options()
+    _chrome_options.add_argument('disable-infobars')
+    _chrome_options.add_argument("user-data-dir={}".format(userProfile))
 
     if request.method == 'POST' and 'url' in request.POST:
         url = request.POST.get('url', '')
-    driver = webdriver.Chrome(DRIVER)
-    driver.get('https://www.wikipedia.com')
-    driver.set_window_size(width, height)
-    now = str(datetime.today().timestamp())
-    img_dir = settings.MEDIA_ROOT
-    img_name = ''.join([now, '_image.png'])
-    full_img_path = os.path.join(img_dir, img_name)
-    if not os.path.exists(img_dir):
-        os.makedirs(img_dir)
-    driver.save_screenshot(full_img_path)
-    screenshot = open(full_img_path, 'rb').read()
-    var_dict = {'screenshot': img_name, 'save': True}
-    links = driver.find_elements_by_tag_name('a') 
-    for link in links:
-        driver.click(link)
-    driver.quit()
+
+        driver = webdriver.Chrome(DRIVER, chrome_options = _chrome_options)
+        driver.get(url)
+
+
+        now = str(datetime.today().timestamp())
+        website = url.replace('/', '.')
+        media_dir = settings.MEDIA_ROOT
+        img_dir = ''.join([now, website])
+        img_paths = []
+        max_runs = 15 #todo make this param
+        for i in range(max_runs):
+            print("i is", i)
+            # driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+            img_name = ''.join([str(i), '_image.png'])
+            full_path = os.path.join(media_dir, img_dir)
+            full_img_path = os.path.join(media_dir, img_dir, img_name)
+            img_paths.append(full_img_path)
+            if not os.path.exists(full_path):
+                os.makedirs(full_path)
+            driver.save_screenshot(full_img_path)
+            screenshot = open(full_img_path, 'rb').read()
+            var_dict = {'screenshot': img_name, 'save': True}
+            # links = driver.find_elements_by_tag_name('a')
+            try:
+                links = driver.find_elements_by_xpath("//a[@href]")
+                num_links = len(links)
+                print(num_links)
+                if num_links == 0:
+                    break
+                index = math.floor((i / max_runs) * num_links)
+                index = random.randint(0, index)
+                if (index == num_links):
+                    index = index-1
+                print(index)
+                attribute = links[index].get_attribute("href")
+                print(attribute) 
+                driver.get(attribute)
+            except:
+                break
+
+        imagelist = [Image.open(path) for path in img_paths]
+        width = 2880
+        height = 1800
+        new_im = Image.new('RGB', (width, height))
+        for i, image in enumerate(imagelist):
+            if i == 0:
+                denom = 2
+            else:
+                denom = max_runs + 1 - i
+            resized_width = width // denom
+            resized_height = height // denom
+            x = random.randint(0, width - resized_width)
+            y = random.randint(0, height - resized_height)
+            resized = image.resize([resized_width,resized_height], Image.ANTIALIAS)
+            new_im.paste(resized, (x, y))
+
+        collage_name = 'collage.png'
+        rel_collage_path = os.path.join(img_dir, collage_name)
+        full_collage_path = os.path.join(media_dir, img_dir, collage_name)
+        all_collages = os.path.join(media_dir, collage_name)
+        var_dict = {'collage': rel_collage_path}
+        new_im.save(full_collage_path)
+        new_im.save(all_collages)
+        driver.quit()
+        os.system('open ' + full_collage_path)
+        print('here')
+        return render(request, 'home.html', var_dict)
+    else:
+        return HttpResponse('Error')
+
+
